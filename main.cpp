c45b448263e3b196b1a270eb50014200ce9dfbd1
Sofia Lisovskaya <LisovskyaSA@mpei.ru>
2022-04-17 21:44:29 +0300

    unit testing

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include "histogram.h"
 
 using namespace std;
 
@@ -15,23 +16,7 @@ input_numbers(size_t count)
     return result;
 }
 
-void
-find_minmax(const vector<double>& numbers, double& min, double& max)
-{
-    min = numbers[0];
-    max = numbers[0];
-    for (double x : numbers)
-    {
-        if (x < min)
-        {
-            min = x;
-        }
-        else if (x > max)
-        {
-            max = x;
-        }
-    }
-}
+
 
 
 vector<size_t>
